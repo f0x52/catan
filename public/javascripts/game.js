e26510412f6f0d069a4ffe72aafea59222e9d9cf
f0x <f@0x52.eu>
2018-11-23 17:17:37 +0100

    client+server mockup

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,82 +1,133 @@
+// All of this would happen serverside
+
 /**
- * Shuffles array in place.
- * @param {Array} a items An array containing the items.
- * Taken from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
  */
-
- function shuffle(a) {
-     var j, x, i;
-     for (i = a.length - 1; i > 0; i--) {
-         j = Math.floor(Math.random() * (i + 1));
-         x = a[i];
-         a[i] = a[j];
-         a[j] = x;
-     }
-     return a;
- }
-
-let board = document.getElementById("board")
-function generateBoard(board) {
-  let row = document.createElement("div")
-  row.className = "row"
-
-  let numbers = []
-  for (let i=2; i<=12; i++) {
-    numbers.push(i)
-    numbers.push(i)
-  }
-  shuffle(numbers)
-
-  let layout = [3, 4, 5, 4, 3]
-  let rowCount = 1
-  let tileCount = 0
-  for(let i=1; i<=19; i++) { //Catan has 19 tiles
-    if (tileCount == layout[0]) {
-      layout.splice(0, 1)
-      board.appendChild(row)
-      rowCount++
-      row = document.createElement("div")
-      row.className = "row"
-      tileCount = 0
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
+}
+
+let game = {
+  tiles: {
+    tileA: {
+      buildings: [0, 1, 2, 8, 9, 10]
+    },
+    tileB: {
+      buildings: [2, 3, 4, 10, 11, 12]
+    },
+    tileC: {
+      buildings: [4, 5, 6, 12, 13, 14]
+    },
+    tileD: {
+      buildings: [7, 8, 9, 7, 8, 9]
+    },
+    tileE: {
+      buildings: [9, 10, 11, 7, 8, 9]
+    },
+    tileF: {
+      buildings: [11, 12, 13, 7, 8, 9]
+    },
+    tileG: {
+      buildings: [13, 14, 15, 7, 8, 9]
+    },
+    tileH: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileI: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileJ: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileK: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileL: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileM: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileN: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileO: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileP: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileQ: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileR: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileS: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    }
+  }
+}
+
+function initResources() {
+  let distribution = {
+    "wood": 4,
+    "brick": 3,
+    "iron": 3,
+    "grain": 4,
+    "wool": 4,
+    "desert": 1
+  }
+  let resources = []
+
+  Object.keys(distribution).forEach((resource) => {
+    for(let i=0; i<distribution[resource]; i++) {
+      resources.push(resource)
+    }
+  })
+  shuffleArray(resources)
+  return resources
+}
+
+let resources = initResources()
+let pos = 0
+Object.keys(game.tiles).forEach((tileName) => {
+  let currentTile = game.tiles[tileName]
+  currentTile.type = resources[pos]
+  pos++
+})
+
+// Actual Clientside
+
+let tilesPerRow = [3, 4, 5, 4, 3]
+let rowCount = 0
+let row = document.createElement("div")
+row.className = "row"
+let tileCount = 0
+
+Object.keys(game.tiles).forEach((tileName) => {
+  let currentTile = game.tiles[tileName]
+
+  if (tileCount == tilesPerRow[rowCount]) {
+    board.appendChild(row)
+    rowCount++
+    row = document.createElement("div")
+    row.className = "row"
+    tileCount = 1
+  } else {
     tileCount++;
-    let tile = generateTile(numbers, i)
-    let rand = Math.floor(Math.random() * 5)
-    if (rand ==1) {
-      tile.className += " forest"
-    } else if (rand==2) {
-      tile.className += " field"
-    } else if (rand==3) {
-      tile.className += " mesa"
-    } else if (rand==4) {
-      tile.className += " mountain"
-    } else {
-      tile.className += " pasture"
-    }
-    row.appendChild(tile)
   }
-  board.appendChild(row)
-}
 
-function generateTile(numbers, pos) {
-  let tilename = "tile"+pos
   let tile = document.createElement("div")
-  tile.className = "tile"
-  tile.id = tilename
-  for(let i=0; i<3; i++) {
-    let part = document.createElement("div")
-    part.className = "tilePart"
-    part.addEventListener("click", () => {console.log(tilename)})
-    tile.appendChild(part)
-  }
-  if (pos == 10) {
-    return tile;
-  }
-  let num = document.createElement("span")
-  num.innerHTML = numbers[pos]
-  num.className="num"
-  tile.appendChild(num)
-  return tile
-}
-
-generateBoard(board);
+  tile.className = "tile " + currentTile.type
+  tile.id = "tile" + tileCount
+  row.appendChild(tile)
+})
+//add last row
+board.appendChild(row)
