98be850b4d888b5e2d8cfe8b9916dc6748823b6a
f0x <f@0x52.eu>
2018-11-28 21:41:08 +0100

    Generating the board instead of hardcoding

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -111,23 +111,97 @@ Object.keys(game.tiles).forEach((tileName) => {
 
 // Actual Clientside
 
-Object.keys(game.tiles).forEach((tileName) => {
-  let currentTile = game.tiles[tileName]
-  console.log(tileName)
-  let tile = document.getElementById(tileName)
-  tile.className += " " + currentTile.type
-  let numberDiv = document.querySelector("#" + tileName + " #num")
-  if (currentTile.number != null) {
-    numberDiv.innerHTML = currentTile.number
-  } else {
-    numberDiv.style.opacity = 0
-  }
-})
+let board = document.getElementById("board")
+let tiles = document.getElementById("tiles")
+let buildings = document.getElementById("buildings")
+let roads = document.getElementById("roads")
+let boardSize = [3,5] //min tiles per row, max tiles per row
+let tileHeight = 160
+let tileWidth = Math.sqrt(3)*tileHeight/2 * (27/26) // texture isn't a perfect hexagon
+let rowDistance = "-"+tileHeight/4+"px"
 
-let buildings = document.getElementsByClassName("building")
-for (let i = 0; i < buildings.length; i++) {
-  buildings[i].addEventListener("click", (event) => {
-    console.log(event.target.id)
-  })
-  buildings[i].innerHTML = buildings[i].id.substr(8)
+let placedTiles = 0
+
+tiles.style.marginTop = rowDistance //compensate for marginBottom on rows
+
+//Top half
+for (let i=boardSize[0]; i<boardSize[1]; i++) {
+  tiles.appendChild(Row(i, "top"))
+}
+
+let middleRow = true
+
+//Middle + Bottom half
+for (let i=boardSize[1]; i>=boardSize[0]; i--) {
+  tiles.appendChild(Row(i, "bottom", middleRow))
+  middleRow = false
+}
+
+function Row(tiles, location, middleRow) {
+  let row = document.createElement("div")
+  row.className = "row"
+  let firstChild = true
+
+  for (let i=0; i<tiles; i++) {
+    let tile = document.createElement("div")
+    let gameTile = game.tiles[Object.keys(game.tiles)[placedTiles]]
+    tile.className = "tile " + gameTile.type
+    tile.style.height = tileHeight+"px"
+    tile.style.width = tileWidth+"px"
+    tile.style.marginRight = tileHeight/15+"px"
+
+    tile.style.gridTemplateRows = 1/4*tileHeight + "px auto " + 1/4*tileHeight + "px"
+
+    for (let j=0; j<3; j++) {
+      tile.appendChild(Building(j, location, firstChild))
+    }
+
+    if (middleRow) {
+      for(let j=0; j<3; j++) {
+        tile.appendChild(Building(j, "top", firstChild))
+      }
+    }
+
+    if (firstChild) {
+      firstChild = false
+    }
+
+    tile.appendChild(Num(gameTile.number))
+
+    row.appendChild(tile)
+    placedTiles++
+  }
+  let offset = (boardSize[1]-tiles) * ( 0.5 * tileWidth + .75*tileHeight/20)
+  row.style.marginLeft = offset + "px"
+  row.style.marginBottom = -tileHeight/4 + tileHeight/10 + "px"
+  return row
+}
+
+function Building(num, location, firstChild) {
+  let building = document.createElement("div")
+  building.className = "building " + location + " pos" + num + strPossibility(12, " red") + strPossibility(25, " blue") + strPossibility(12, " orange") + strPossibility(12, " white") + strPossibility(25, " city")
+  building.style.gridArea = location+num
+  if (num==0 && !firstChild) {
+    building.style.opacity = 0;
+    building.style.zIndex = -100;
+  }
+  return building
+}
+
+function Num(num) {
+  let number = document.createElement("div")
+  number.className = "number"
+  if (num != undefined) {
+    number.innerHTML = num
+  } else {
+    number.style.opacity = 0
+  }
+  return number
+}
+
+function strPossibility(odds, str) {
+  if(Math.random()*100 < odds) {
+    return str
+  }
+  return ""
 }
