9bfb61843a3554136ee59ca12cc0213dad3ae539
Lars <lvblokland@gmail.com>
2018-12-03 15:27:24 +0100

    server and lobby

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var http = require("http")
 var websocket = require("ws")
+var createLobby = require("./server/lobby")
 
 var port = process.argv[2]
 var app = express()
@@ -21,95 +22,16 @@ const wss = new websocket.Server({
 
 let lobbies = []
 
-let lobby = {
-
-
-
-}
-
-function createLobby(){
-
-  let board = require("./server/gameBoard.js")
-
-  /**
-   * Randomize array element order in-place.
-   * Using Durstenfeld shuffle algorithm.
-   */
-
-  function shuffleArray(array) {
-      for (var i = array.length - 1; i > 0; i--) {
-          var j = Math.floor(Math.random() * (i + 1));
-          var temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-      }
-  }
-
-
-
-  function initResources() {
-    let distribution = {
-      "wood": 4,
-      "brick": 3,
-      "iron": 3,
-      "grain": 4,
-      "wool": 4,
-      "desert": 1
-    }
-
-    let resources = []
-
-    Object.keys(distribution).forEach((resource) => {
-      for(let i=0; i<distribution[resource]; i++) {
-        resources.push(resource)
-      }
-    })
-    shuffleArray(resources)
-    return resources
-  }
-
-  let resources = initResources()
-  let numbers = []
-  for(let i=2; i<=12; i++) {
-    if (i==7) {
-      continue
-    }
-    numbers.push(i);
-    if (i != 2 && i != 12) {
-      numbers.push(i)
-    }
-  }
-  shuffleArray(numbers)
-  let pos = 0
-  let numPos = 0
-  let usedNumbers = {}
-  Object.keys(board.tiles).forEach((tileName) => {
-    let currentTile = board.tiles[tileName]
-    currentTile.type = resources[pos]
-    if (resources[pos] != "desert") {
-      currentTile.number = numbers[numPos]
-      numPos++
-    }
-    pos++
-  })
-
-  let colors = ["red", "blue", "white", "orange"]
-  shuffleArray(colors)
-
-  let lobby = {
-    started: false,
-    players: [],
-    currentPlayer: 0,
-    board: board,
-    colors: colors
-  }
-
-  return lobby
-}
-
-console.log(createLobby())
-
 wss.on("connection", function(ws) {
+
+let lobby 
+
+if(lobbies.length == 0){
+  lobbies.push(createLobby())
+}
+
+if(lobbies[lobbies.length-1].players.)
+
 
   let playerNum
 
