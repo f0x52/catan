f2fe8ddb7ed4b82e7615b60e0b874491ae4cfd45
Lars <lvblokland@gmail.com>
2018-12-03 16:43:47 +0100

    gameBoard now with correct numbers

--- server/gameBoard.js
+++ server/gameBoard.js
@@ -10,52 +10,52 @@ module.exports = {
       buildings: [4, 5, 6, 12, 13, 14]
     },
     tileD: {
-      buildings: [7, 8, 9, 7, 8, 9]
+      buildings: [7, 8, 9, 20, 21, 24]
     },
     tileE: {
-      buildings: [9, 10, 11, 7, 8, 9]
+      buildings: [9, 10, 11, 24, 25, 28]
     },
     tileF: {
-      buildings: [11, 12, 13, 7, 8, 9]
+      buildings: [11, 12, 13, 28, 29, 32]
     },
     tileG: {
-      buildings: [13, 14, 15, 7, 8, 9]
+      buildings: [13, 14, 15, 32, 33, 36]
     },
     tileH: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [19, 20, 21, 16, 17, 18]
     },
     tileI: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [21, 24, 25, 18, 22, 23]
     },
     tileJ: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [25, 28, 29, 23, 26, 27]
     },
     tileK: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [29, 32, 33, 27, 30, 31]
     },
     tileL: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [33, 36, 37, 31, 34, 35]
     },
     tileM: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [17, 18, 22, 38, 39, 40]
     },
     tileN: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [22, 23, 26, 40, 41, 42]
     },
     tileO: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [26, 27, 30, 42, 43, 44]
     },
     tileP: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [30, 31, 34, 44, 45, 46]
     },
     tileQ: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [39, 40, 41, 47, 48, 49]
     },
     tileR: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [41, 42, 43, 49, 50, 51]
     },
     tileS: {
-      buildings: [0, 1, 2, 7, 8, 9]
+      buildings: [43, 44, 45, 51, 52, 53]
     }
   }
 }
