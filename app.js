34a37f8a9eb034f12e2a30d7b2a91dc06c53d302
Lars <lvblokland@gmail.com>
2018-12-03 15:08:32 +0100

    app.js changed to accommodate boards.

--- app.js
+++ app.js
@@ -19,12 +19,95 @@ const wss = new websocket.Server({
   server
 })
 
-let colors = ["red", "blue", "white", "orange"]
+let lobbies = []
 
-let connections = []
-let players = []
-let currentPlayer = 0
-let gameStart = false
+let lobby = {
+
+
+
+}
+
+function createLobby(){
+
+  let board = require("./server/gameBoard.js")
+
+  /**
+   * Randomize array element order in-place.
+   * Using Durstenfeld shuffle algorithm.
+   */
+
+  function shuffleArray(array) {
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+  }
+
+
+
+  function initResources() {
+    let distribution = {
+      "wood": 4,
+      "brick": 3,
+      "iron": 3,
+      "grain": 4,
+      "wool": 4,
+      "desert": 1
+    }
+
+    let resources = []
+
+    Object.keys(distribution).forEach((resource) => {
+      for(let i=0; i<distribution[resource]; i++) {
+        resources.push(resource)
+      }
+    })
+    shuffleArray(resources)
+    return resources
+  }
+
+  let resources = initResources()
+  let numbers = []
+  for(let i=2; i<=12; i++) {
+    if (i==7) {
+      continue
+    }
+    numbers.push(i);
+    if (i != 2 && i != 12) {
+      numbers.push(i)
+    }
+  }
+  shuffleArray(numbers)
+  let pos = 0
+  let numPos = 0
+  let usedNumbers = {}
+  Object.keys(board.tiles).forEach((tileName) => {
+    let currentTile = board.tiles[tileName]
+    currentTile.type = resources[pos]
+    if (resources[pos] != "desert") {
+      currentTile.number = numbers[numPos]
+      numPos++
+    }
+    pos++
+  })
+
+  let colors = ["red", "blue", "white", "orange"]
+  shuffleArray(colors)
+
+  let lobby = {
+    started: false,
+    players: [],
+    currentPlayer: 0,
+    board: board,
+    colors: colors
+  }
+
+  return lobby
+}
+
+console.log(createLobby())
 
 wss.on("connection", function(ws) {
 
---
+++ server/gameBoard.js
@@ -0,0 +1,61 @@
+module.exports = {
+  tiles: {
+    tileA: {
+      buildings: [0, 1, 2, 8, 9, 10]
+    },
+    tileB: {
+      buildings: [2, 3, 4, 10, 11, 12]
+    },
+    tileC: {
+      buildings: [4, 5, 6, 12, 13, 14]
+    },
+    tileD: {
+      buildings: [7, 8, 9, 7, 8, 9]
+    },
+    tileE: {
+      buildings: [9, 10, 11, 7, 8, 9]
+    },
+    tileF: {
+      buildings: [11, 12, 13, 7, 8, 9]
+    },
+    tileG: {
+      buildings: [13, 14, 15, 7, 8, 9]
+    },
+    tileH: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileI: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileJ: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileK: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileL: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileM: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileN: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileO: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileP: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileQ: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileR: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    },
+    tileS: {
+      buildings: [0, 1, 2, 7, 8, 9]
+    }
+  }
+}
